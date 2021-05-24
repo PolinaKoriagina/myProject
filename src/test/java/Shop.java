1338f4003c8d2d15974fc0200a102a5750d028ec
PolinaKoriagina <koriagina.p@gmail.com>
2021-05-24 18:46:37 +0200

    Initial commit

---
+++ src/test/java/Shop.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+public class Shop {
+    @Test
+    void addDressToCart(){
+        open("http://automationpractice.com/index.php");
+        
+
+    }
+}
