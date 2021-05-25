b01ef3ab0f10bab5f88350b46a8796fecff0e9b7
PolinaKoriagina <koriagina.p@gmail.com>
2021-05-25 14:34:27 +0200

    added a lipstick

--- src/test/java/Shop.java
+++ src/test/java/Shop.java
@@ -1,13 +1,14 @@
+
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static com.codeborne.selenide.Selectors.by;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Condition.text;
+
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -22,18 +23,18 @@ public class Shop {
     @AfterAll
     static void tearDown() {
         logger.info("@AfterAll method");
-        logger.info("The form is submitted :)");
+        logger.info("We added a lipstick:)");
     }
     @Test
-    void openTheBeauty(){
+    void addLipstick(){
         open("https://www.zara.com/pl/");
         $("#onetrust-accept-btn-handler").click();
         $("[aria-label=\"Otworzyć menu\"]").click();
         $("[aria-label=\"BEAUTY, Rozwiń kategorie pomocnicze\"]").click();
         $("[aria-label=\"KOLEKCJA, Rozwiń kategorie pomocnicze\"]").click();
-//        $(By.xpath("//div[6]/ul[1]/li[2]")).hover();
-//       $(byText("Dresses")).click();
-//        System.out.println("Click is confirmed");
+        $("[alt=\"STILETTO DEMI-MATTE LIPSTICK – WKŁAD\"]").scrollIntoView(true).click();
+        $("#product-78478936").shouldHave(text("Dodaj do koszyka"));
+
 
     }
 }
