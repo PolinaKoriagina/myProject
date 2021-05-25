9191b7afe18febf9bdc3d71845e38fe9c9ff147e
PolinaKoriagina <koriagina.p@gmail.com>
2021-05-25 13:48:56 +0200

    added opening the page

--- src/test/java/Shop.java
+++ src/test/java/Shop.java
@@ -1,12 +1,39 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class Shop {
+    static Logger logger = LoggerFactory.getLogger(Shop.class);
+    @BeforeAll
+    static void setUp() {
+        logger.info("@BeforeAll method");
+        Configuration.startMaximized = true;
+        Configuration.browser = "chrome";
+    }
+    @AfterAll
+    static void tearDown() {
+        logger.info("@AfterAll method");
+        logger.info("The form is submitted :)");
+    }
     @Test
-    void addDressToCart(){
-        open("http://automationpractice.com/index.php");
-        
+    void openTheBeauty(){
+        open("https://www.zara.com/pl/");
+        $("#onetrust-accept-btn-handler").click();
+        $("[aria-label=\"Otworzyć menu\"]").click();
+        $("[aria-label=\"BEAUTY, Rozwiń kategorie pomocnicze\"]").click();
+        $("[aria-label=\"KOLEKCJA, Rozwiń kategorie pomocnicze\"]").click();
+//        $(By.xpath("//div[6]/ul[1]/li[2]")).hover();
+//       $(byText("Dresses")).click();
+//        System.out.println("Click is confirmed");
 
     }
 }
